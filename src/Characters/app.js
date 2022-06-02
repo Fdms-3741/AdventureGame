3c98bd5ee2528e72af43d0e4a61d71db0ebe7435
Fernando Dias <fernandodias@gta.ufrj.br>
2022-06-02 11:00:05 -0300

    WIP: Microsserviço de personagem

---
+++ src/Characters/app.js
@@ -0,0 +1,31 @@
+/*
+ * app.js
+ *
+ * Description: This file implements the CRUD application that manages Characters 
+ *
+ *
+ * */
+
+/* Applictin */
+
+/* Used libraries*/
+const express = require('express') /* HTTP Handler */
+const mongo = require('mongoose') /* API for integration with MongoDB database */
+const amqp = require('amqplib/callback_api'); /* For integration with RabbitMQ message broker */
+
+/* Reads from dotenv file in project's root folder. Overwrites already defined ones */
+require('dotenv').config()
+
+/* Constants */
+const GatewayAddress  = "http://gateway/"
+const MongoDBAddress  = "http://" + process.env.MONGODB_USER + ":" + process.env.MONGODB_PASSWORD +"@characterdb/" /* Gets username and password from environment variables set in the container */
+const RabbitMQAddress = "http://broker/"
+
+/* Create application */
+const app = express()
+
+/* Connect to MongoDB database */
+const db = mongo.connect(GatewayAddress+"characters/")
+
+
+/* Defining REST application */
