fead861d5bda669d7345e4cc39250a35d87e7045
Fdms-3741 <fernando.dias.3741@gmail.com>
2022-06-04 13:55:22 -0300

    Muda mongoose para mongo, que é mais simples

--- src/Characters/app.js
+++ src/Characters/app.js
@@ -10,7 +10,7 @@
 
 /* Used libraries*/
 const express = require('express') /* HTTP Handler */
-const mongo = require('mongoose') /* API for integration with MongoDB database */
+const mongo = require('mongodb') /* API for integration with MongoDB database */
 const amqp = require('amqplib/callback_api'); /* For integration with RabbitMQ message broker */
 
 /* Reads from dotenv file in project's root folder. Overwrites already defined ones */
@@ -18,7 +18,7 @@ require('dotenv').config()
 
 /* Constants */
 const GatewayAddress  = "http://gateway/"
-const MongoDBAddress  = "http://" + process.env.MONGODB_USER + ":" + process.env.MONGODB_PASSWORD +"@characterdb/" /* Gets username and password from environment variables set in the container */
+const MongoDBAddress  = "mongodb+srv://" + process.env.MONGODB_USER + ":" + process.env.MONGODB_PASSWORD +"@characterdb/character" /* Gets username and password from environment variables set in the container */
 const RabbitMQAddress = "http://broker/"
 
 /* Create application */
