a0ee27bf40aefc927a63c15a5a8d8159f4c08b31
Fdms-3741 <fernando.dias.3741@gmail.com>
2022-06-21 22:59:47 -0300

    mtas mods

--- src/Characters/src/__tests__/app.test.ts
+++ src/Characters/src/__tests__/app.test.ts
@@ -1,21 +1,15 @@
-import { MONGODB_TEST_ADDRESS } from '../defaults'
+import { MONGODB_TEST_ADDRESS, ClearDatabase,CloseDatabase,InitializeDatabase } from '../defaults'
 import request from 'supertest'
 import app from '../app'
 import  mongoose from 'mongoose'
 
-async function Connect(){
-	await mongoose.connect(MONGODB_TEST_ADDRESS,{
-		authSource:"admin"
-	}).then(()=>{console.log("connected to the database")})
-}
-
 beforeAll(async () => {
-	await Connect()
+	await InitializeDatabase(MONGODB_TEST_ADDRESS)
 }) 
 
 afterAll(async () => {
-	await mongoose.connection.dropDatabase()
-	await mongoose.connection.close()
+	await ClearDatabase()
+	await CloseDatabase()
 })
 
 
@@ -31,35 +25,65 @@ describe("Database connection conditions",()=>{
 	})
 })
 
-describe("GET /", () => {
-	it("Should connect", async () => {
-		let response = await request(app).get("/")
-		expect(response?.statusCode).toBe(200)
-		expect(response?.text).toStrictEqual("Hello there")
-	})
-})
-
-describe("GET /character", ()=>{
-	it("Should return 200",async () => {
-		let response = await request(app).get("/character").send({"user_id":3,"name":"francis"})
-		console.log(response.text)
-		expect(response?.statusCode).toBe(200)
-	})
-})
-
 describe("POST /character", () => {
 	it("Should return 200", async () =>{
-		let response = await request(app).post("/character").send({"user_id":"aaaaaaaaaaaaaaaaaaaaaaaa","name":"francis","description":"the hero's mission"})
+		let response = await request(app).post("/characters").send({"user_id":"aaaaaaaaaaaaaaaaaaaaaaaa","name":"francis","description":"the hero's mission"})
 		console.log(response.text)
 		expect(response?.statusCode).toBe(200)
 	})
 
 	it("Should fail due to not having name", async () => {
-		let response = await request(app).post('/character').send({"user_id":"aaaaaaaaaaaaaaaaaaaaaaaa",'description':"123"})
+		let response = await request(app).post('/characters').send({"user_id":"aaaaaaaaaaaaaaaaaaaaaaaa",'description':"123"})
 		console.log(response.body)
 		expect(response?.statusCode).toBe(400)
-		expect(response?.body.message).toStrictEqual("Data failed to match schema.")
+		expect(response?.body.message).toStrictEqual("Data failed to match schema")
 	})
 })
 
+describe("GET /character", ()=>{
+	it("Should return list of characters",async () => {
+		let response = await request(app).get("/characters").send()
+		console.log(response.text)
+		expect(response?.statusCode).toBe(200)
+		expect(response?.body).toBeInstanceOf(Array)
+	})
+})
 
+describe("GET /character/:id",()=>{
+	let userId = "aaaaaaaaaaaaaaaaaaaaaaaa";
+	let charName = "Will them";
+
+	it("should place a char in the database",async () => {
+	})
+
+	it("Should return lists for an specific Id",async () => {
+		let placement = await request(app).post('/characters').send({
+			user_id:userId,
+			name:charName,
+			description:"Valorous sup"
+		})
+		let charId = placement.body._id
+		let response = await request(app).get(`/characters/${userId}`)
+		console.log(response.body)
+		expect(response?.statusCode).toBe(200)
+		expect(response.body).toBeInstanceOf(Array)
+	})
+
+	it("Should return only one character", async ()=>{
+		let placement = await request(app).post('/characters').send({
+			user_id:userId,
+			name:charName,
+			description:"Valorous sup"
+		})
+		let charId = placement.body._id
+		let response = await request(app).get(`/characters/${userId}/${charId}`)
+		expect(response?.statusCode).toBe(200)
+		expect(response.body._id).toStrictEqual(charId)
+		expect(response.body.name).toStrictEqual(charName)
+	})
+})
+
+describe("PUT /characters",()=>{
+	let userId = "AAaaAaAa"
+	let charName = 
+})
