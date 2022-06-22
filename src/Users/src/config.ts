008e093d42ac720cf4e5f54d1dc61ca0988f5f3e
Rnagao <nagao1234@poli.ufrj.br>
2022-06-22 18:48:15 -0300

    solve conflict docker-compose

--- src/Users/src/config.ts
+++ src/Users/src/config.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
-const SERVER_PORT = process.env.SERVER_PORT || '80';
+const SERVER_PORT = '3000';
 
 const SERVER = {
     hostname: SERVER_HOSTNAME,
