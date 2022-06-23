24b8e119c2c2c3023371422f03326425d4fe3b5f
Fdms-3741 <>
2022-06-23 10:48:06 -0300

    Integração do jupyter

--- docker/microservice.Dockerfile
+++ docker/microservice.Dockerfile
@@ -20,8 +20,12 @@ ARG PROJECT_ROOT_DIRECTORY
 
 COPY ${PROJECT_ROOT_DIRECTORY} /app/
 
+WORKDIR /app/
+
+RUN rm package-lock.json
+
 RUN npm i
 
 RUN npm run build --if-present
 
-CMD [ "npm","start" ]
+CMD [ "npm","run","start" ]
--- src/Characters/src/server.ts
+++ src/Characters/src/server.ts
@@ -1,3 +1,7 @@
 import app from './app'
+import mongoose from 'mongoose';
+import * as defaults from './defaults'
 
-app.listen(80)
+defaults.InitializeDatabase(defaults.MONGODB_ADDRESS).then(()=>{console.log("Connected to the database"),() => {throw Error("Couldn't connect to the database")}})
+
+app.listen(80,"characters")
